363bd5e63e38cbd18501e191d5e37de7acc29bb4
qiule chen <q.l.chen@student.tue.nl>
2022-03-15 21:02:49 +0100

    tweaks

--- scripts.js
+++ scripts.js
@@ -2,8 +2,17 @@ let darkTheme = localStorage.getItem('dark-theme') === 'true';
 let correctWord;
 let row = 1;
 let guess = '';
+let complete = false;
+let startTime = 0;
+let minutes = 0;
+let seconds = 0;
+let time_taken = '';
+let totalTime = 0;
 
 function onKeyPress(key) {
+    if (complete === true) {
+        return;
+    }
     let charnum = key.keyCode;
     if (guess.length > 0 && key.key == 'Backspace') {
         let word = document.getElementById('row' + row).children[guess.length - 1];
@@ -14,15 +23,17 @@ function onKeyPress(key) {
 
     if (guess.length === 5 && key.key === 'Enter' && allWords.includes(guess)) {
         validateWord();
-        if(guess === correctWord)
+        if (guess === correctWord)
             completeGame(true);
         row += 1;
         guess = '';
+        if (row === 7) {
+            completeGame(false)
+        }
         return;
     }
 
     if (guess.length < 5) {
-        console.log(key);
         if ((charnum > 96 && charnum < 123) || (charnum > 64 && charnum < 91)) {
             guess = guess + key.key;
             let word = document.getElementById('row' + row).children[guess.length - 1];
@@ -53,9 +64,22 @@ function validateWord() {
     }
 }
 
-function completeGame(win){
+function completeGame(win) {
+    complete = true;
+    totalTime = Date.now() - startTime;
+    minutes = Math.floor((totalTime) / 60000);
+    seconds = Math.floor(totalTime / 1000) % 60;
+    time_taken = (minutes > 0 ? minutes + 'm ' : '') + seconds + 's';
+
+
+    let time = document.getElementById('completion-time');
+    time.innerHTML = time_taken;
+    let word = document.getElementById('correct-word');
+    word.innerHTML = correctWord;
     let resultsPanel = document.getElementById('results-panel');
+    resultsPanel.classList.add(win ? 'won' : 'lost');
     resultsPanel.classList.add('visible');
+
 }
 
 
@@ -63,8 +87,9 @@ document.addEventListener('keydown', onKeyPress);
 
 window.onload = () => {
     // Pick a word
-    correctWord = allWords[Math.floor(Math.random()*allWords.length)].toUpperCase();
-    // correctWord = allWords[Date.now() % 1000 * 60 * 60 * 24 % allWords.length].toUpperCase();
+    correctWord = allWords[Math.floor(Math.random() * allWords.length)].toUpperCase();
+    // correctWord = allWords[Date.now() % (1000 * 60 * 60 * 24) % allWords.length].toUpperCase();
+    startTime = Date.now();
 
     // Restore dark theme settings
     updateDarkTheme();
